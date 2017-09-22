6d95000fdf1796ec4e66a425312e5ef4d938a425
Max Scher <maxobaxo@gmail.com>
2017-09-22 12:56:41 -0700

    refactor component tree to minimize class-based components

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -2,17 +2,41 @@ import React from 'react';
 import PrelimFormControl from './PrelimFormControl';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import c from './../constants';
 
 class Create extends React.Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.showFormModal = this.showFormModal.bind(this);
+    this.hideFormModal = this.hideFormModal.bind(this);
+  }
+
+  showFormModal(event) {
+    event.preventDefault();
+    const { dispatch } = this.props;
+    const action = {
+      type: c.SHOW_ALIGN_MODAL
+    }
+    dispatch(action);
+  }
+
+  hideFormModal() {
+    const { dispatch } = this.props;
+    const action = {
+      type: c.HIDE_ALIGN_MODAL
+    }
+    dispatch(action);
   }
 
   render() {
     return(
       <div>
-        <PrelimFormControl/>
+        <PrelimFormControl
+          showFormModal={this.showFormModal}
+          hideFormModal={this.hideFormModal}
+          isModalVisible={this.props.masterState.isModalVisible}
+        />
       </div>
     );
   }
--- src/components/PrelimFormControl.jsx
+++ src/components/PrelimFormControl.jsx
@@ -1,60 +1,34 @@
 import React from 'react';
 import PrelimForm from './PrelimForm';
-import { connect } from 'react-redux';
-import { Modal } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
-import c from './../constants';
+import { Modal } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
-class PrelimFormControl extends React.Component {
+function PrelimFormControl(props) {
 
-  constructor(props) {
-    super(props);
-    this.showFormModal = this.showFormModal.bind(this);
-    this.hideFormModal = this.hideFormModal.bind(this);
-  }
-
-  showFormModal(event) {
-    event.preventDefault();
-    const { dispatch } = this.props;
-    const action = {
-      type: c.SHOW_ALIGN_MODAL
-    }
-    dispatch(action);
-  }
-
-  hideFormModal() {
-    const { dispatch } = this.props;
-    const action = {
-      type: c.HIDE_ALIGN_MODAL
-    }
-    dispatch(action);
-  }
-
-  render() {
-    return(
-      <div>
-        <Button onClick={this.showFormModal}>
-          Begin Character Creation
-        </Button>
-        <Modal
-          show={this.props.masterState.isModalVisible}
-          onHide={this.hideFormModal}>
-          <Modal.Header>
-            <Modal.Title>Choose Your Alignment</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <PrelimForm hideFormModal={this.hideFormModal}/>
-          </Modal.Body>
-        </Modal>
-      </div>
-    )
-  }
+  return(
+    <div>
+      <Button onClick={props.showFormModal}>
+        Begin Character Creation
+      </Button>
+      <Modal
+        show={props.isModalVisible}
+        onHide={props.hideFormModal}>
+        <Modal.Header>
+          <Modal.Title>Choose Your Alignment</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <PrelimForm hideFormModal={props.hideFormModal}/>
+        </Modal.Body>
+      </Modal>
+    </div>
+  )
 }
 
-const mapStateToProps = state => {
-  return {
-    masterState : state
-  }
+PrelimFormControl.propTypes = {
+  showFormModal: PropTypes.func,
+  hideFormModal: PropTypes.func,
+  isModalVisible: PropTypes.bool
 }
 
-export default connect(mapStateToProps)(PrelimFormControl);
+export default PrelimFormControl;

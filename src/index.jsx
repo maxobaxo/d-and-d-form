35b6f145eca54695940f3269f3a6347c28d4911e
Max Scher <maxobaxo@gmail.com>
2017-09-22 10:00:28 -0700

    complete entry point index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+import reducer from './reducers/main-reducer';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+
+const store = createStore(reducer);
+
+RenderDOM.render(
+  <Provider store={store}>
+    <HashRouter>
+      <App/>
+    </HashRouter>
+  </Provider>,
+  document.getElementById('react-app-root');
+);
